943e5eba69dd00bc67af984540663f06c84ba914
EugeneLys <eugene.lysakov@gmail.com>
2022-03-20 23:55:21 +0500

    4.1. Разбор первой программы. [#159033 #277934]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         System.out.println("Eugene Lysakov");
         System.out.println("01.01.2021");
     }
