3456b0ef76ea9febd13bae5389bdbc00d2c61aae
EugeneLys <eugene.lysakov@gmail.com>
2022-03-31 15:50:02 +0500

    4.3.3. Логическое отрицание ! [#334072]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -4,10 +4,10 @@ public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
         if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
-            } else {
-                System.out.println("I can't.");
-            }
+        } else {
+            System.out.println("I can't.");
         }
+    }
 
     public static void main(String[] args) {
         Club.permission(true, true);
