19cc65d3cf2b6480a4e731cb1984703833bf2115
EugeneLys <eugene.lysakov@gmail.com>
2022-04-09 22:41:06 +0500

    5.9. Крест в псевдографике. [#177182 #286538]

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -4,8 +4,8 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = (cell == row);
-                boolean right = (cell == size - 1 - row);
+                boolean left = cell == row;
+                boolean right = cell == size - 1 - row;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
