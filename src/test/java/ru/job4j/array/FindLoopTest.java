04e47cbaa379ad3360977d886c3aa1cb57f290df
Евгений <eugene.lysakov@gmail.com>
2022-04-15 17:22:15 +0500

    6. Checkstyle [#4123 #278225]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -10,7 +10,7 @@ public class FindLoopTest {
     public void whenArrayHasMinus6Then5() {
         int[] data = {5, 4, 3, 2, 100, -6};
         int el = -6;
-        int result = FindLoop.indexOf(data,-6);
+        int result = FindLoop.indexOf(data, -6);
         int expected = 5;
         Assert.assertEquals(expected, result);
     }
@@ -19,7 +19,7 @@ public class FindLoopTest {
     public void whenArrayHas5Then0() {
         int[] data = {5, 4, 3, 2};
         int el = 5;
-        int result = FindLoop.indexOf(data,5);
+        int result = FindLoop.indexOf(data, 5);
         int expected = 0;
         Assert.assertEquals(expected, result);
     }
@@ -28,7 +28,7 @@ public class FindLoopTest {
     public void whenArrayHasNot10ThenMinus1() {
         int[] data = {6, 5, 4, 3, 2};
         int el = 10;
-        int result = FindLoop.indexOf(data,10);
+        int result = FindLoop.indexOf(data, 10);
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
