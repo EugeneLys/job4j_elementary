63dfeebe7cd90d80c4b46458f40c707368614a24
Евгений <eugene.lysakov@gmail.com>
2022-04-16 13:36:20 +0500

    6.2.2. Переставить элементы массива [#226795 #289365]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -10,7 +10,7 @@ public class SwitchArrayTest {
     public void whenSwap0To3() {
         int[] input = {1, 2, 3, 4};
         int source = 0;
-        int dest = input.length - 1;
+        int dest = 3;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {4, 2, 3, 1};
         Assert.assertArrayEquals(expected, result);
@@ -19,8 +19,8 @@ public class SwitchArrayTest {
     @Test
     public void whenSwapSecondToPenult() {
         int[] input = {1, 2, 3, 4, 5, 6};
-        int source = input.length - (input.length - 1);
-        int dest = input.length - 2;
+        int source = 1;
+        int dest = 4;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {1, 5, 3, 4, 2, 6};
         Assert.assertArrayEquals(expected, result);
@@ -29,8 +29,8 @@ public class SwitchArrayTest {
     @Test
     public void whenSwapThirdToThirdFromTheEnd() {
         int[] input = {1, 2, 3, 4, 5, 6};
-        int source = input.length - (input.length - 2);
-        int dest = input.length - 3;
+        int source = 2;
+        int dest = 3;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {1, 2, 4, 3, 5, 6};
         Assert.assertArrayEquals(expected, result);
