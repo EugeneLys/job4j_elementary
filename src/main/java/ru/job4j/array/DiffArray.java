54b7f9f4f2a2371e1b448aca4c0670c6d18c8470
EugeneLys <eugene.lysakov@gmail.com>
2022-05-04 22:56:39 +0500

    DiffArray

---
+++ src/main/java/ru/job4j/array/DiffArray.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+//Разность множеств это операция при которой в результирующее множество
+// попадают элементы из первого множества, но только если их нет во втором множестве.
+
+public class DiffArray {
+    public static int[] diff(int[] left, int[] right) {
+
+        int[] temp = new int[left.length];
+        int t = 0;
+        if (left.length != 0 && right.length != 0) {
+            for (int i = 0; i < left.length; i++) {
+                for (int k : right) {
+                    if (left[i] == k) {
+                        break;
+                    } else {
+                        temp[t] = left[i];
+                        t++;
+                    }
+                }
+            }
+        }
+
+        if (right.length == 0) {
+            for (int j : left) {
+                temp[t] = j;
+                t++;
+            }
+        }
+
+        int[] result = new int[t];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+
+    }
+}
