aaf52e6e000f5032214e670cc6006f4ad8cdc0d6
Евгений <eugene.lysakov@gmail.com>
2022-05-11 14:15:59 +0500

    Anomaly solution

--- src/main/java/ru/job4j/array/Anomaly.java
+++ src/main/java/ru/job4j/array/Anomaly.java
@@ -4,26 +4,30 @@ import java.util.Arrays;
 
 public class Anomaly {
     public static int[][] found(int[] data, int up, int down) {
-        int[][] result = new int[data.length][];
+        int[][] result = new int[data.length][2];
         int count = 0;
         int totalCount = 0;
         int[] temp = new int[data.length];
-        int t = 0;
-        int r = 0;
-
-        for (int i = r; r < data.length; i++) {
-            while (data[i] <= 4 || data[i] >= 16) {
+        int i = 0;
+        while (i < data.length) {
+            while (data[i] <= down || data[i] >= up) {
                 temp[count] = i;
                 count++;
-                i++;
-                r++;
+                if (i < data.length - 1) {
+                    i++;
+                } else {
+                    break;
+                }
             }
             if (count != 0) {
-                result[totalCount] = Arrays.copyOf(temp, count);
-                r++;
+                int[] temp2 = new int[2];
+                temp2[0] = temp[0];
+                temp2[1] = temp[count - 1];
+                result[totalCount] = Arrays.copyOf(temp2, 2);
                 count = 0;
                 totalCount++;
             }
+            i++;
         }
         return Arrays.copyOf(result, totalCount);
     }
