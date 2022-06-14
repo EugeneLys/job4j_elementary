55fefdc354ab9e60bb8b0ede75edfab0d6948ee2
Евгений <eugene.lysakov@gmail.com>
2022-06-14 15:38:28 +0500

    1. Чтение кода и базовые ошибки [#40205 #311642]

--- src/main/java/ru/job4j/array/SymmetricDiff.java
+++ src/main/java/ru/job4j/array/SymmetricDiff.java
@@ -4,78 +4,30 @@ import java.util.Arrays;
 
 public class SymmetricDiff {
     public static int[] diff(int[] left, int[] right) {
-        int[] rightTemp = new int[right.length];
-        int[] leftTemp = new int[left.length];
-        int[] temp = new int[left.length + right.length];
-        int t = 0;
-        int l = 0;
-        int r = 0;
-
-        if (left.length == 0 && right.length == 0) {
-            return temp;
-        }
-
-        if (left.length == 0) {
-            for (int j : right) {
-                temp[t] = j;
-                t++;
-            }
-        }
         if (right.length == 0) {
-            for (int j : left) {
-                temp[t] = j;
-                t++;
+            return left;
+        }
+        int[] rsl = new int[left.length + right.length];
+        int size = 0;
+        for (int k : left) {
+            int count = 0;
+            for (int j = 0; j < right.length; j++) {
+                if (k == right[j]) {
+                    right[j] = 0;
+                    break;
+                }
+                count++;
+                if (count == right.length) {
+                    rsl[size++] = k;
+                }
+
             }
         }
-
-        if (left.length != 0 && right.length != 0) {
-            for (int i = 0; i < left.length; i++) {
-                for (int j = 0; j < right.length; j++) {
-                    if (right[j] == left[i]) {
-                        rightTemp[r] = j;
-                        leftTemp[l] = i;
-                        r++;
-                        l++;
-                    }
-                }
+        for (int i : right) {
+            if (i != 0) {
+                rsl[size++] = i;
             }
-
-            if (r > 0) {
-                for (int i = 0; i < r; i++) {
-                    for (int j = 0; j < right.length; j++) {
-                        if (j != rightTemp[i]) {
-                            temp[t] = right[j];
-                            t++;
-                        }
-                    }
-                }
-            } else {
-                for (int i : right) {
-                    temp[t] = i;
-                    t++;
-                }
-            }
-
-            if (l > 0) {
-                for (int i = 0; i < l; i++) {
-                    for (int j = 0; j < left.length; j++) {
-                        if (j != leftTemp[i]) {
-                            temp[t] = left[j];
-                            t++;
-                        }
-                    }
-                }
-            } else {
-                for (int i : left) {
-                    temp[t] = i;
-                    t++;
-                }
-            }
-
         }
-            int[] result = new int[t];
-            System.arraycopy(temp, 0, result, 0, result.length);
-            Arrays.sort(result);
-            return result;
+        return Arrays.copyOf(rsl, size);
     }
 }
